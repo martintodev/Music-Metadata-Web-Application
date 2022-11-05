a717a1649b6dade0f4976162c5e1a9642e05f8c0
Martin Todev <mtodev@uwo.ca>
2022-11-05 04:19:21 -0400

    Added GET artist details and GET track detials

--- index.js
+++ index.js
@@ -5,14 +5,34 @@ const port = 3000;
 const fs = require('fs');
 const csv = require('csv-parser');
 
-let genreData, newArray = [];
+let genreDataFinal, genreDataInitial = [];
+let artistDataFinal, artistDataInitial = [];
+let trackDataFinal, trackDataInitial = [];
 
+fs.createReadStream('lab3-data/raw_artists.csv')
+.pipe(csv())
+.on('data', (rows) => {
+    artistDataInitial.push(rows)
+    artistDataFinal = artistDataInitial.map((rows) => {
+        return {
+            'artist_id':rows.artist_id,
+            'artist_handle':rows.artist_handle,
+            'artist_name':rows.artist_name,
+            'artist_date_created':rows.artist_date_created,
+            'artist_location':rows.artist_location,
+            'artist_favorites':rows.artist_favorites,
+            'tags':rows.tags
+        }
+    });
+})
+.on ('end', () => {
+});
 
 fs.createReadStream('lab3-data/genres.csv')
 .pipe(csv())
 .on('data', (rows) => {
-    newArray.push(rows)
-    genreData = newArray.map((rows) => {
+    genreDataInitial.push(rows)
+    genreDataFinal = genreDataInitial.map((rows) => {
         return {
             'genre_id':rows.genre_id,
             'parent_id':rows.parent,
@@ -22,6 +42,31 @@ fs.createReadStream('lab3-data/genres.csv')
 })
 .on ('end', () => {
 });
+
+fs.createReadStream('lab3-data/raw_tracks.csv')
+.pipe(csv())
+.on('data', (rows) => {
+    trackDataInitial.push(rows)
+    trackDataFinal = trackDataInitial.map((rows) => {
+        return {
+            'track_id':rows.track_id,
+            'album_id':rows.album_id,
+            'album_title':rows.album_title,
+            'artist_id':rows.artist_id,
+            'artist_name':rows.artist_name,
+            'tags':rows.tags,
+            'track_date_created':rows.track_date_created,
+            'track_date_recorded':rows.track_date_recorded,
+            'track_duration':rows.track_duration,
+            'track_genres':rows.track_genres,
+            'track_number':rows.track_number,
+            'track_title':rows.track_title
+        }
+    });
+})
+.on ('end', () => {
+});
+
 
 
 
@@ -36,9 +81,30 @@ app.use((req, res, next) => { //For all routes
 
 //Gets genres
 app.get('/api/genres', (req, res) => {
-    res.send(genreData);
+    res.send(genreDataFinal);
 });
 
+//Gets artist from artist ID
+app.get('/api/artists/:artist_id', (req, res) => {
+    const id = req.params.artist_id;
+    const artist = artistDataFinal.find(p => p.artist_id === id);
+    if(artist) {
+        res.send(artist);
+    } else {
+        res.status(404).send(`Artist with ID ${id} was not found`);
+    }
+});
+
+//Get track details from track ID
+app.get('/api/tracks/:track_id', (req, res) => {
+    const id = req.params.track_id;
+    const track = trackDataFinal.find(p => p.track_id === id);
+    if(track) {
+        res.send(track);
+    } else {
+        res.status(404).send(`Track with ID ${id} was not found`);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
