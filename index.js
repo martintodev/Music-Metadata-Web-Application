1a81d346c7271bc0c38a2c0abc2d684d330ca643
Martin Todev <mtodev@uwo.ca>
2022-11-05 03:22:44 -0400

    Added parse for genre and GET for genre

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const router = express.Router();
+
+const fs = require('fs');
+const csv = require('csv-parser');
+
+let genreData, newArray = [];
+
+
+fs.createReadStream('lab3-data/genres.csv')
+.pipe(csv())
+.on('data', (rows) => {
+    newArray.push(rows)
+    genreData = newArray.map((rows) => {
+        return {
+            'genre_id':rows.genre_id,
+            'parent_id':rows.parent,
+            'title':rows.title
+        }
+    });
+})
+.on ('end', () => {
+});
+
+
 
 //Setup serving front-end code
 app.use('/', express.static('static'));
@@ -12,6 +34,12 @@ app.use((req, res, next) => { //For all routes
     next(); //Keep going
 });
 
+//Gets genres
+app.get('/api/genres', (req, res) => {
+    res.send(genreData);
+});
+
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-});
+}); 
