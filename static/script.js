620977acbf893ab6908c085d8a4ae4b16e3d4403
Martin Todev <mtodev@uwo.ca>
2022-11-09 23:33:16 -0500

    Final Commit

--- static/script.js
+++ static/script.js
@@ -3,8 +3,11 @@ document.getElementById('searchArtist').addEventListener('click', getByArtistNam
 document.getElementById('searchAlbum').addEventListener('click', getByAlbumName);
 document.getElementById('addPlaylist').addEventListener('click', addNewPlaylist);
 document.getElementById('refreshPlaylists').addEventListener('click', showAllPlaylists);
+document.getElementById('addToPlaylist').addEventListener('click', updatePlaylist);
+document.getElementById('deletePlaylist').addEventListener('click', deletePlaylist);
 
 let chosenPlaylist = '';
+let chosenTracks = [];
 
 showAllPlaylists();
 
@@ -29,6 +32,36 @@ function showAllPlaylists() {
         });
     })
     )
+}
+
+function updatePlaylist() {
+    fetch(`/api/playlist/${chosenPlaylist}`, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(chosenTracks)
+    })
+    .then(res => {
+        if(res.ok)
+        res.json()
+        .then(data => console.log(data))
+        .catch(console.log('Failed to get json object'))
+    })
+    .catch()
+
+    const t = document.getElementById('addTracks');
+    t.replaceChildren('');
+}
+
+function deletePlaylist() {
+    fetch(`/api/playlist/${chosenPlaylist}`, {
+        method: 'DELETE'
+    })
+    .then(res => {
+        res.json()
+        .then(data => console.log(data))
+        .catch(console.log('Failed to get json object'))
+    })
+    .catch()
 }
 
 
@@ -111,6 +144,25 @@ function getPlaylistData(playlist) {
     )
 }
 
+function addToPlaylist(track) {
+    let currentPlaylist = '';
+    const t = document.getElementById('addTracks');
+    const row = document.createElement('tr');
+    const item = document.createElement('th');
+
+    if(currentPlaylist != chosenPlaylist) {
+        chosenTracks = [];
+        currentPlaylist = chosenPlaylist;
+    }
+
+    item.appendChild(document.createTextNode(`Track_id is: ${track}`));
+    row.appendChild(item);
+    t.appendChild(row);
+
+    chosenTracks.push(track);
+    
+}
+
 
 function populateTable(data) {
     const t = document.getElementById('playlistTracks');
@@ -124,12 +176,18 @@ function populateTable(data) {
                     const row = t.insertRow(count);
                     row.classList.add("data");
                     var cell1 = row.insertCell(0);
+                    
                     cell1.classList.add("heading_num");
                     var cell2 = row.insertCell(1);
                     var cell3 = row.insertCell(2);
                     var cell4 = row.insertCell(3);
                     var cell5 = row.insertCell(4);
                     var cell6 = row.insertCell(5);
+
+                    cell6.addEventListener("click", function(){
+                        addToPlaylist(cell1.innerText);
+                    });
+
                     cell1.innerText = data.track_id;
                     cell2.innerText = '';
                     cell3.innerText = data.track_title;
